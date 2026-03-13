9cd97f82cc54448560c51703fe5be153980c86f0
Andreiyiii <andrei230834@gmail.com>
2026-03-13 21:16:07 +0200

    testing motors

---
+++ test_motors.ino
@@ -0,0 +1,35 @@
+#include <Servo.h>
+
+Servo motorPan;  // Pinul 9 (Stanga-Dreapta)
+Servo motorTilt; // Pinul 10 (Sus-Jos)
+
+void setup() {
+  motorPan.attach(9);
+  motorTilt.attach(10);
+}
+
+void loop() {
+ 
+  motorTilt.write(90); 
+  
+  motorPan.write(0);
+  delay(1500);
+  
+  motorPan.write(180);
+  delay(1500);
+  
+  motorPan.write(90); 
+  delay(1000);
+
+
+  
+  
+  motorTilt.write(45);
+  delay(1500);
+  
+  motorTilt.write(120);
+  delay(1500);
+  
+  motorTilt.write(90); 
+  delay(1000);
+}
